5b94085b0742ef30524d94b3475544b6f711d934
Massimiliano Adamo <maxadamo@gmail.com>
2014-06-29 20:28:37 +0200

    Update galera_params.py

--- files/galera_params.py
+++ files/galera_params.py
@@ -1,6 +1,20 @@
-# The following 3 lines come from Puppet
+# The following lines come from Puppet
+#
+# Comma separated list of all hosts in the cluster
 all_nodes = ["<%= @galera_hosts.join('", "') -%>"]
+
+# Users and passwords
 credentials = {"root": "<%= @galera_root_password %>", "sstuser": "<%= @galera_sst_password %>", "nagios": "<%= @galera_nagios_password %>"}
-mydomain = ".<%= @domain -%>"
-# The above 3 lines come from Puppet
+
+# Domain name of your hosts
+domain = ".<%= @domain -%>"
+
+<% if @galera_vendor == 'mariadb' -%>
+#This command is used with MariaDB
+ootstrap_cmd = "bootstrap"
+<% else -%>
+# This command is used with Percona
+bootstrap_cmd = "bootstrap-pxc
+<% end -%>
+# The above lines come from Puppet
 
